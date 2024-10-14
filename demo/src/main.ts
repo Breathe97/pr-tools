fe469514c91d6d2f060e341e93a1b4113cf8de40
Breathe97 <704340916@qq.com>
2024-10-14 15:30:50 +0800

    a

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -9,58 +9,9 @@ import { uuid, random, randomName, md5, regExps, timeStamp, timeFormat, timeFrom
 
 // uuid
 {
-  let str = uuid(32, 16)
-  prTools.uuid()
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:uuid`, str)
-}
-{
-  // random
-  let str = random(-1, 999999) // 6位数字
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:random`, str)
-}
-{
-  // randomName
-  let str = randomName(2, 4) // 2-4位昵称
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:randomName`, str)
-}
-// md5
-{
-  let str = md5('123456')
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:md5`, str)
-}
-
-// regExps
-{
-  let str = regExps.mobile.test('222')
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:regExps`, str)
-}
-
-// timeStamp
-{
-  let str = timeStamp('2024/09/24 04:06:06')
+  let str = timeStamp('2024-10-08T06:00:00Z') // 1728396000000 1728367200000
+  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:str`, str)
+  // str = timeFormat(str, 'YYYY-MM-DD hh:mm:ss')
+  str = timeFormat('2024-10-08 06:00:00', 'YYYY-MM-DD hh:mm:ss')
   console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:timeStamp`, str)
-}
-// timeFormat
-{
-  let str = timeFormat('2024/09/24 04:06:06', 'YYYY-MM-DD hh:mm:ss')
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:timeFormat`, str)
-}
-// timeFrom
-{
-  let str = timeFrom(new Date().getTime() - 5600000)
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:timeFrom`, str)
-}
-// line2hump
-{
-  let str = prTools.line2hump('asd-daa')
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:line2hump`, str)
-}
-// hump2line
-{
-  let str = prTools.hump2line('AddaDdd')
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:test:hump2line`, str)
-}
-{
-  let res = prTools.bytesFormat(22222)
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:res`, res)
 }
