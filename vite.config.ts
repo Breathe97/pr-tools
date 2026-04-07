f9b5aaa3d78722c05f8be01181c8210af2942de3
Breathe <704340916@qq.com>
2026-04-07 10:03:16 +0800

    d

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 export default defineConfig({
-  plugins: [dts({ rollupTypes: false })],
+  plugins: [dts({ rollupTypes: false, insertTypesEntry: false })],
   build: {
     lib: {
       entry: 'src/index.ts',
